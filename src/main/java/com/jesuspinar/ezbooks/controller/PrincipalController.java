360a5d3975a181809cb04ae0a13e120acfcad73e
Jesus <jesuspl@tutanota.com>
2022-12-23 20:04:23 +0100

    Add login form

--- src/main/java/com/jesuspinar/ezbooks/controller/PrincipalController.java
+++ src/main/java/com/jesuspinar/ezbooks/controller/PrincipalController.java
@@ -2,7 +2,6 @@ package com.jesuspinar.ezbooks.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Actua como mapeador para las peticiones de usuarios no loggeados
@@ -13,5 +12,9 @@ public class PrincipalController {
     @GetMapping("/index")
     private String goToIndex(){
         return "index";
+    }
+    @GetMapping("/login")
+    private String goToLogin(){
+        return "login";
     }
 }
